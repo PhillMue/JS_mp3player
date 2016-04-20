453c4a40ba3d9a9b3a5314d965b36cb67a905a25
PhillMue <muenzuve@gmail.com>
2016-04-20 17:45:32 +0300

    completed js and edited html

--- foundation-6/js/app.js
+++ foundation-6/js/app.js
@@ -1,1 +1,49 @@
-$(document).foundation();
+// JavaScript Document
+
+var dir = "mp3/";
+var playlist = ["sample", "sample2", "sample3"];
+var playlist_index = 0;
+var ext = ".mp3";
+var audio = new Audio();
+audio.src = dir + playlist[0] + ext;
+audio.loop = false;
+audio.autoplay = false;
+audio.play();
+
+var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
